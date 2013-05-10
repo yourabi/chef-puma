75f6de14ae0e4c37aaae2bfbb2a750b2936b3b02
Yousef Ourabi <yourabi@gmail.com>
2013-05-10 13:41:52 -0700

    correct/enhance metadata

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,25 @@ name             'puma'
 maintainer       'Yousef Ourabi'
 maintainer_email 'yourabi@gmail.com'
 license          'Apache v2.0'
-description      'Installs/Configures puma'
+description      'Installs and configures puma'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe           "puma", "Install puma"
+
 depends          'monit'
 depends          'logrotate'
+
+%w{ ubuntu debian }.each do |os|
+  supports os
+end
+
+attribute "puma/version",
+  :display_name => "Puma Version",
+  :description => "Puma Version to install",
+  :default => "2.0.1"
+
+attribute "puma/bundler_version",
+  :display_name => "Bundler Version",
+  :description => "Bundler Version to install",
+  :default => "1.3.5"
