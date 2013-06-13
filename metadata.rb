512d928a345f05d3119bebb331836ac516c1c147
Aubrey Holland <aubreyholland@gmail.com>
2013-06-13 08:52:11 -0400

    bump the version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'yourabi@gmail.com'
 license          'Apache v2.0'
 description      'Installs and configures puma'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 recipe           "puma", "Install puma"
 
