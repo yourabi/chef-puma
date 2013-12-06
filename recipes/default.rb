5af1d4746a83dd82a532481b61cc1947cd3ce13e
Yousef Ourabi <yourabi@gmail.com>
2013-12-06 14:02:52 -0800

    add foodcritic and start cleaning up warnings

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,12 +5,12 @@
 #
 
 gem_package 'bundler' do
-  version "#{node.puma[:bundler_version]}"
-  gem_binary "#{node.puma[:rubygems_location]}"  
+  version node.puma[:bundler_version]
+  gem_binary node.puma[:rubygems_location]  
   options '--no-ri --no-rdoc'
 end
 
 gem_package 'puma' do
   action :install
-  version "#{node.puma[:version]}"
+  version node.puma[:version]
 end
