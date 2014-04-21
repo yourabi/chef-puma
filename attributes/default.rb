d60844b49c538c5ff99b46481a79d5b0d0b6f407
Yousef Ourabi <yourabi@gmail.com>
2014-04-21 15:34:50 -0700

    bumping puma to version 2.8.2 and bundler to 1.6.2 closing #9

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default.puma[:version] = "2.7.0"
-default.puma[:bundler_version] = "1.3.5"
+default.puma[:version] = "2.8.2"
+default.puma[:bundler_version] = "1.6.2"
 default.puma[:rubygems_location] = "/usr/local/bin/gem"
