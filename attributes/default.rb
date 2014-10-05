f6a06ae7c6bcac51e4626203acdb666e56bfe700
Yousef Ourabi <yourabi@gmail.com>
2014-10-04 22:14:40 -0700

    bump default bundler version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default["puma"]["version"] = "2.9.1"
-default["puma"]["bundler_version"] = "1.7.2"
+default["puma"]["bundler_version"] = "1.7.3"
 default["puma"]["rubygems_location"] = "/usr/local/bin/gem"
