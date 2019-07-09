e271aa3d66e378fa5315be95d03c6d2439ed85e0
Adam Crownoble <adam@codenoble.com>
2019-07-08 18:40:26 -0700

    Remove the now invalid calls to --no-ri and --no-rdoc
    They've been replaced by `--no-document` but for better compatibility,
    I think it's best to have none of these options set at all, as the
    defaults should be fine.
    
    See https://github.com/rubygems/rubygems/pull/2354

--- definitions/puma_install.rb
+++ definitions/puma_install.rb
@@ -1,9 +1,8 @@
 define :puma_install, gem_bin_path: "/usr/local/bin/gem" do
-                                         
+
   gem_package 'bundler' do
     version node["puma"]["bundler_version"]
     gem_binary params[:gem_bin_path]
-    options '--no-ri --no-rdoc'
   end
 
   gem_package 'puma' do
